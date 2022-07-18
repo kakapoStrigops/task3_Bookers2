4fe2ae2c1c63e7696638a18f9155513557830212
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-18 07:17:20 +0000

    [Update] bookのindexページの作成、create機能の作成、bookのshowページの作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,31 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
-    @user = current_user
     @book = Book.new
+    @user = current_user
+    @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(params[:id])
+    else
+      render :books
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/home/about" => "homes#about", as: "about"
 
-  resources :books, only: [:new, :index, :show, :edit]
+  resources :books, only: [:create, :index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
 
 end
