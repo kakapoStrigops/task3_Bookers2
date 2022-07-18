ac45709fd3bf25b0752b674b4388d37eb992c398
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-18 09:03:54 +0000

    [Update] showページ作成(show,edit,destroyリンクの作成前)、create機能のリダイレクト先の修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(params[:id])
+      redirect_to book_path(@book.id)
     else
       render :books
     end
@@ -19,6 +19,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
+    @book_new = Book.new
   end
 
   def edit
