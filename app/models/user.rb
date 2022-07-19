af377958dadb402e0d42abcddc7db08174ea6266
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-19 10:44:57 +0000

    [Update] バリデーションの実装(ユーザのプロフィール更新失敗時)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
+  validates :name, length: {minimum: 2, maximum: 20}
+
   has_one_attached :profile_image
 
   def get_profile_image(width, height)
