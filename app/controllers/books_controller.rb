4302eb510e1a060627127fc20a6bf60640824545
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-19 11:34:08 +0000

    [Update] アクセス制限の実装(他人の投稿編集画面に遷移できない)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @book = Book.new
@@ -46,6 +47,12 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 
 end
