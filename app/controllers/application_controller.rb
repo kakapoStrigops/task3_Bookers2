77543f1bc8e8537a8a263caba9f5e22fcb2af31e
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-19 09:00:57 +0000

    [Update] フラッシュメッセージの実装(ユーザログイン・ユーザ新規登録・ユーザログアウトの成功時)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
     user_path(current_user)
   end
 
+
+
   def after_sign_out_path_for(resource)
-    flash[:notice] = "Signed out successfully."
     root_path
   end
 
